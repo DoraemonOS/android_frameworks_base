a8843d4cef79fcaada0b9aa300f7ed65f3faf0ea
jhenrique09 <jhenrique09.mcz@hotmail.com>
2022-02-22 14:59:10 +0900

    SystemUI: Block face authentication when device is in pocket mode
    Use pocket lock to dertermine if the device is in pocket, don't listen
    for face authentication if so. This helps not triggering failed attempts
    
    Stallix: Adapt from A10
    
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2372,7 +2372,7 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
                 && !mKeyguardGoingAway && biometricEnabledForUser && !mLockIconPressed
                 && strongAuthAllowsScanning && mIsPrimaryUser
                 && (!mSecureCameraLaunched || mOccludingAppRequestingFace)
-                && !faceAuthenticated;
+                && !faceAuthenticated && !mIsDeviceInPocket;
 
         if (shouldListen && mFaceUnlockBehavior == FACE_UNLOCK_BEHAVIOR_SWIPE && !mBouncerFullyShown){
             shouldListen = false;
